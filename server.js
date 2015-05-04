225e496bfab6e3dcdec9ad4ee80d9f2cb57bff86
natsuyuu <seiinatsuyuu@gmail.com>
2015-05-04 21:28:11 +0800

    aaa

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ var OpenshiftApp = function() {
        
         connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
             if (err) throw err;
-            console.log('The solution is: ',、 rows[0].solution);
+            console.log('The solution is: ',rows[0].solution);
         });
     };
     
